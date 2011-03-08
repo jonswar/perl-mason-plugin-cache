b4e409316aeff53c20008cc1b8d4ec34933738f2
Jonathan Swartz <swartz@pobox.com>
2011-03-07 21:49:32 -0800

    0.03

--- lib/Mason/Plugin/Cache/Interp.pm
+++ lib/Mason/Plugin/Cache/Interp.pm
@@ -5,8 +5,8 @@ use Mason::PluginRole;
 
 # Passed attributes
 #
-has 'cache_defaults'   => ( isa => 'HashRef', lazy_build => 1 );
-has 'cache_root_class' => ( isa => 'Str', default => 'CHI' );
+has 'cache_defaults'   => ( is => 'ro', isa => 'HashRef', lazy_build => 1 );
+has 'cache_root_class' => ( is => 'ro', isa => 'Str', default => 'CHI' );
 
 method _build_cache_defaults () {
     return {
