377fe8d169a12de9960c69d04dd86c79bbcabc5f
Jonathan Swartz <swartz@pobox.com>
2011-06-23 12:52:48 -0700

    assign Default key by default

--- lib/Mason/Plugin/Cache/Filters.pm
+++ lib/Mason/Plugin/Cache/Filters.pm
@@ -2,6 +2,7 @@ package Mason::Plugin::Cache::Filters;
 use Mason::PluginRole;
 
 method Cache ( $key, $set_options, %cache_options ) {
+    $key = 'Default' if !defined($key);
     Mason::DynamicFilter->new(
         filter => sub {
             $self->cache(%cache_options)->compute( $key, $_[0], $set_options );
